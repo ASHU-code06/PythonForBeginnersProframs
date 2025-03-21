7b008368b4fb2432b4c8fcc46c9172f2cfa43a90
ASHU-code06 <ashuc306@gmail.com>
2025-03-21 10:25:47 +0530

    a selection statement program -> if-else condition

---
+++ ifstatementsprogram.py
@@ -0,0 +1,10 @@
+a=33
+b=220
+if b > a:
+    print("b is greater than a \n")
+elif a == b:
+        print("a and b are same ")
+
+#identation -> In computer programming, indentation is the use of white space of
+#           -> tabs at the beginning of a line of code to organize and visually group the code.
+#           ->  It is used to make the code easier to understand and more readable
